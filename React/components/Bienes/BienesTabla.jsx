f77e77e5d306bf7300cdde55add58460f3ce83d0
emohedano <emohedano@switchfly.com>
2020-04-30 23:19:29 -0500

    Se agrega paginacion a componente de tabla

--- React/components/Bienes/BienesTabla.jsx
+++ React/components/Bienes/BienesTabla.jsx
@@ -3,10 +3,10 @@ import Funciones from '../Funciones'
 import GenericTable from '../commons/GenericTable';
 
 const COLUMNS = [
-    {field:"fgarIdFideicomiso", header:"Fideicomiso"},
+    {field:"fgarIdFideicomiso", header:"Fideicomiso", isKey: true},
     {field:"fgarCveGarantia2", header:"Tipo de Bien"},
-    {field:"fgarImpGarantiaFormatted", header:"Importe"},
-    {field:"fgarImpGarantizadFormatted", header:"Importe Garantizado"},
+    {field:"fgarImpGarantiaFormatted", header:"Importe", numeric: true},
+    {field:"fgarImpGarantizadFormatted", header:"Importe Garantizado", numeric: true},
     {field:"fgarCveStatus", header:"Status"}
 ];
 
@@ -15,20 +15,102 @@ class BienesApi extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            datos:[],
-            electo:null
+            datos:[
+                {
+                    "fgarImpGarantiaFormatted": "         245,834,654.77",
+                    "fgarImpGarantizad": 245834654.77,
+                    "fgarIdFideicomiso": 3623,
+                    "fgarCveGarantia2": "OTROS BIENES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 3,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "         245,834,654.77",
+                    "fgarImpGarantia": 245834654.77,
+                    "fgarImpGarantizadFormatted": "         245,834,654.77",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                },
+                {
+                    "fgarImpGarantiaFormatted": "         316,608,415.65",
+                    "fgarImpGarantizad": 316608415.65,
+                    "fgarIdFideicomiso": 3624,
+                    "fgarCveGarantia2": "INMUEBLES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 2,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "         316,608,415.65",
+                    "fgarImpGarantia": 316608415.65,
+                    "fgarImpGarantizadFormatted": "         316,608,415.65",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                },
+                {
+                    "fgarImpGarantiaFormatted": "         131,316,738.50",
+                    "fgarImpGarantizad": 131316738.5,
+                    "fgarIdFideicomiso": 3625,
+                    "fgarCveGarantia2": "INMUEBLES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 2,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "         131,316,738.50",
+                    "fgarImpGarantia": 131316738.5,
+                    "fgarImpGarantizadFormatted": "         131,316,738.50",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                },
+                {
+                    "fgarImpGarantiaFormatted": "           4,229,232.03",
+                    "fgarImpGarantizad": 4229232.03,
+                    "fgarIdFideicomiso": 3627,
+                    "fgarCveGarantia2": "INMUEBLES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 2,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "           4,229,232.03",
+                    "fgarImpGarantia": 4229232.03,
+                    "fgarImpGarantizadFormatted": "           4,229,232.03",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                },
+                {
+                    "fgarImpGarantiaFormatted": "             979,792.04",
+                    "fgarImpGarantizad": 979792.04,
+                    "fgarIdFideicomiso": 3632,
+                    "fgarCveGarantia2": "OTROS BIENES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 3,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "             979,792.04",
+                    "fgarImpGarantia": 979792.04,
+                    "fgarImpGarantizadFormatted": "             979,792.04",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                },
+                {
+                    "fgarImpGarantiaFormatted": "         104,484,030.22",
+                    "fgarImpGarantizad": 104484030.22,
+                    "fgarIdFideicomiso": 3634,
+                    "fgarCveGarantia2": "INMUEBLES",
+                    "fgarCveStatus": "ACTIVO",
+                    "fgarCveGarantia": 2,
+                    "fgarIdSubcuenta": 0,
+                    "disponibleBien": "         104,484,030.22",
+                    "fgarImpGarantia": 104484030.22,
+                    "fgarImpGarantizadFormatted": "         104,484,030.22",
+                    "pagosBien": "                   0.00",
+                    "ctoNumEscritura": null
+                }
+            ],
+            electo: null
         };
     }
 
     async componentDidMount() {
         var llamados = new Funciones();
 
-        let datos=await llamados.datosTabla('json={"id":"funRegistroGarantias"}');
-        console.log(datos);
-        this.setState({datos:datos});
-        /*this.llamada().then(data => {
-            this.setState({fiduca: data});
-        });*/
+        //let datos = await llamados.datosTabla('json={"id":"funRegistroGarantias"}');
+        //this.setState({datos:datos});
+
     }
 
     displaySelection(data) {
--- React/components/commons/GenericTable.jsx
+++ React/components/commons/GenericTable.jsx
@@ -1,52 +1,222 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const useStyles = makeStyles({
-    root: {
-        width: '100%'
+import PropTypes from 'prop-types';
+
+const SORT_TYPES = {
+    ASC: 'asc',
+    DESC: 'desc'
+}
+
+const ROW_HEIGHT = 53;
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      fontWeight: theme.typography.fontWeightBold
+    }
+}))(TableCell);
+
+const useStyles = makeStyles((theme) => {
+    return {
+        root: {
+            width: '100%',
+            marginBottom: theme.spacing(2),
+        },
+        table: {
+            width: '100%'
+        }
     }
 });
 
-function createHeaders(columns){
-    return columns.map((col) => <TableCell>{col.header}</TableCell>);
-}
+function toggleSelectedElementInList(selectedElements, id){
+    const selectedIndex = selectedElements.indexOf(id);
+    let newSelected = [];
 
-function createRow(row, columns, index){
+    if (selectedIndex === -1) {
+        newSelected = [...selectedElements, id];
+    } else {
+        newSelected = newSelected.concat(
+            selectedElements.slice(0, selectedIndex),
+            selectedElements.slice(selectedIndex + 1),
+        );
+    }
+
+    return newSelected;
+}   
+
+function EnhancedTableHead(props) {
+    const { columns, classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+
+    function createHeader(column){
+        const sortActive = orderBy === column.field;
+        return (
+            <StyledTableCell
+              key={column.field}
+              align={column.numeric ? 'right' : 'left'}
+              sortDirection={sortActive ? order : false}
+            >
+              <TableSortLabel
+                active={sortActive}
+                direction={sortActive ? order : SORT_TYPES.ASC}
+                onClick={createSortHandler(column.field)}
+              >
+                {column.header}
+              </TableSortLabel>
+            </StyledTableCell>
+        )
+    }
+
+    const headers = columns.map(createHeader);
+  
     return (
-        <TableRow key={index}>
-            {columns.map((col) => {
-                return <TableCell>{row[col.field]}</TableCell>
-            })}
+      <TableHead>
+        <TableRow>
+          <StyledTableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={rowCount > 0 && numSelected === rowCount}
+              onChange={onSelectAllClick}
+            />
+          </StyledTableCell>
+          {headers}
         </TableRow>
+      </TableHead>
     );
-}
+  }
+  
+  EnhancedTableHead.propTypes = {
+    columns: PropTypes.array.isRequired,
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf([SORT_TYPES.ASC, SORT_TYPES.ASC]).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
 
-export default function GenericTable (props) {
+
+function GenericTable (props) {
     const {data, columns, onSelect} = props;
-    const classes = useStyles();
+    const keyColum = columns.find((col) => col.isKey).field;
+    const [order, setOrder] = React.useState(SORT_TYPES.ASC);
+    const [orderBy, setOrderBy] = React.useState(keyColum);
+    const [page, setPage] = React.useState(0);
+    const [selected, setSelected] = React.useState([]);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-    const headers = createHeaders(columns);
-    const rows = data.map((row, index) => createRow(row, columns, index));
+    const classes = useStyles();
+    const isSelected = (id) => selected.indexOf(id) !== -1;
+
+    const currentRows = data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+    let rows = currentRows.map((row, index) => createRow(row, columns, index));
+    const emptyRowsCount = rowsPerPage - currentRows.length;
+    let emptyRowsPlaceholder = null;
+
+    if (emptyRowsCount > 0) {
+        emptyRowsPlaceholder = (
+            <TableRow style={{ height: ROW_HEIGHT * emptyRowsCount }}>
+                <TableCell colSpan={columns.length + 1} />
+            </TableRow>
+        );
+    }
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === SORT_TYPES.ASC;
+        setOrder(isAsc ? SORT_TYPES.ASC : SORT_TYPES.ASC);
+        setOrderBy(property);
+      };
+    
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = data.map((row) => row[keyColum]);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, id) => {
+        let newSelected = toggleSelectedElementInList(selected, id);
+        setSelected(newSelected);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    function createRow(row, columns, index){
+        const keyField = row[keyColum];
+        const isItemSelected = isSelected(keyField);
+        return (
+            <TableRow key={keyField} onClick={(event) => handleClick(event, keyField)}>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        checked={isItemSelected}
+                    />
+                </TableCell>
+                {columns.map((col) => {
+                    return <StyledTableCell align={col.numeric ? 'right' : 'left'}>{row[col.field]}</StyledTableCell>
+                })}
+            </TableRow>
+        );
+    }
 
     return (
-        <TableContainer component={Paper} className={classes.root}>
-            <Table aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    {headers}
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Paper className={classes.root}>
+            <TableContainer className={classes.table}>
+                <Table>
+                    <EnhancedTableHead
+                        columns={columns}
+                        classes={classes}
+                        numSelected={selected.length}
+                        order={order}
+                        orderBy={orderBy}
+                        onSelectAllClick={handleSelectAllClick}
+                        onRequestSort={handleRequestSort}
+                        rowCount={rows.length}
+                    />
+                    <TableBody>
+                        {rows}
+                        {emptyRowsPlaceholder}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={data.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
     );
 }
+
+GenericTable.propTypes = {
+    data: PropTypes.array.isRequired, 
+    columns: PropTypes.object.isRequired, 
+    onSelect: PropTypes.func.isRequired
+};
+
+export default GenericTable;
