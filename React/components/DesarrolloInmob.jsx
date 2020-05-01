4e82aaa1e5da325302f7210a6cbcc2befaeb4628
Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>
2020-05-01 13:15:20 -0500

    Cambio de etiquetas en menu
    Signed-off-by: Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>

--- React/components/DesarrolloInmob.jsx
+++ React/components/DesarrolloInmob.jsx
@@ -5,6 +5,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import MainAdquirientes from './Adquirientes/MainAdquirientes';
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -66,34 +68,22 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         className={classes.tabs}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
+        <Tab label="Individualización de Inmuebles" {...a11yProps(0)} />
+        <Tab label="Adquirientes" {...a11yProps(1)} />
+        <Tab label="Liberaciones" {...a11yProps(2)} />
+        <Tab label="Carga Masiva de Inmuebles" {...a11yProps(3)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        Item One
+        Individualización de Inmuebles
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+        {MainAdquirientes}
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Item Three
+        Liberaciones
       </TabPanel>
       <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
+        Carga Masiva de Inmuebles
       </TabPanel>
     </div>
   );
