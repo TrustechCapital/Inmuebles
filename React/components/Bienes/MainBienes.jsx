6e4ed8e935f4ebbd68e3e487b59d30eef7aed08c
emohedano <emohedano@switchfly.com>
2020-05-02 22:11:53 -0500

    Se agrega panel de busqueda para tablas

--- React/components/Bienes/MainBienes.jsx
+++ React/components/Bienes/MainBienes.jsx
@@ -3,16 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import SearchIcon from '@material-ui/icons/Search';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
 
 import Tabla from './BienesTabla';
 import BienesDialog from './BienesDialog';
@@ -20,8 +22,37 @@ import { OPERACIONES_CATALOGO } from '../../constantes.js';
 
 
 const useStyles = makeStyles((theme) => ({
-    searchCard: {
-        borderColor: theme.palette.secondary.main
+    root: {
+        width: '100%',
+    },
+    heading: {
+        marginLeft: '.5rem'
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+    },
+    icon: {
+        verticalAlign: 'bottom',
+        height: 20,
+        width: 20,
+    },
+    details: {
+        alignItems: 'center',
+    },
+    column: {
+        flexBasis: '33.33%',
+    },
+    helper: {
+        borderLeft: `2px solid ${theme.palette.divider}`,
+        padding: theme.spacing(1, 2),
+    },
+    link: {
+        color: theme.palette.primary.main,
+        textDecoration: 'none',
+        '&:hover': {
+            textDecoration: 'underline',
+        },
     },
     rowSpacing: {
         marginBottom: theme.spacing(2)
@@ -63,44 +94,55 @@ export default function Menu(){
     return(
         <div>
             <Grid container spacing={1} direction="column" className={classes.rowSpacing}>
-                <Card className={classes.searchCard} variant="outlined">
-                    <CardContent>
-                        <Grid container spacing={1} direction="column">
-                            <Grid container className={classes.rowSpacing} spacing={4}>
-                                <Grid item xs={4}>
-                                    <TextField id="paramIdFideicomiso" label="Id Fideicomiso" defaultValue="1" fullWidth/>
+                <div className={classes.root}>
+                    <ExpansionPanel defaultExpanded elevation={3}>
+                        <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1c-content"
+                        id="panel1c-header"
+                        >
+                            <Grid container spacing={1} direction="row" alignItems="center">
+                                <SearchIcon fontSize="small"/>
+                                <Typography variant="subtitle1" className={classes.heading}>Parametros de busqueda</Typography>
+                            </Grid>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails className={classes.details}>
+                            <Grid container spacing={1} direction="column">
+                                <Grid container className={classes.rowSpacing} spacing={4}>
+                                    <Grid item xs={3}>
+                                        <TextField id="paramIdFideicomiso" label="Id Fideicomiso" fullWidth/>
+                                    </Grid>
+                                    <Grid container xs={9} alignItems="center">
+                                        <Container></Container>
+                                    </Grid>
                                 </Grid>
-                                <Grid container xs={8} alignItems="center">
-                                    <Container>Fideicomiso 1</Container>
+                                <Grid container spacing={4}>
+                                    <Grid item xs={2}>
+                                        <TextField id="paramSubfiso" label="Id Subcuenta" fullWidth/>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <FormControl fullWidth>
+                                            <InputLabel htmlFor="tipoBien">Tipo de Bien</InputLabel>
+                                            <Select labelId="tipoBien" displayEmpty>
+                                                <MenuItem value=""></MenuItem>
+                                                <MenuItem value={1}>Tipo 1</MenuItem>
+                                                <MenuItem value={2}>Tipo 2</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={7}>
+                                        <TextField id="paramDescripcion" label="Descripcion" fullWidth/>
+                                    </Grid>
                                 </Grid>
                             </Grid>
-                            <Grid container spacing={4}>
-                                <Grid item xs={2}>
-                                    <TextField id="paramSubfiso" label="Id Subcuenta" defaultValue="1" fullWidth/>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <FormControl fullWidth>
-                                        <InputLabel htmlFor="tipoBien">Tipo de Bien</InputLabel>
-                                        <Select labelId="tipoBien" value={1}>
-                                            <MenuItem value={1}>Tipo 1</MenuItem>
-                                            <MenuItem value={2}>Tipo 2</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <TextField id="paramDescripcion" label="Descripcion" fullWidth/>
-                                </Grid>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelActions>
+                            <Grid container justify="flex-end">
+                                <Button variant="contained" color="secondary" startIcon={<SearchIcon />}>Buscar</Button>
                             </Grid>
-                        </Grid>
-                    </CardContent>
-                    <CardActions>
-                        <Grid container justify="flex-end">
-                            <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group">
-                                <Button color="secondary" startIcon={<SearchIcon />}>Buscar</Button>
-                            </ButtonGroup>
-                        </Grid>
-                    </CardActions>
-                </Card>
+                        </ExpansionPanelActions>
+                    </ExpansionPanel>
+                </div>
             </Grid>
             
             <Grid container spacing={1}>
