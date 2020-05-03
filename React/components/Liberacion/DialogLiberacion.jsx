6a23c3820300519b58cd1c03d6d30c3f92d9efe5
Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>
2020-05-03 18:07:16 -0500

    Grid Liberacion
    Signed-off-by: Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>

--- React/components/Liberacion/DialogLiberacion.jsx
+++ React/components/Liberacion/DialogLiberacion.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-import GenericSwitch from '../commons/GenericSwitch';
 import CatalogDialog from '../commons/CatalogDialog';
 import GenericTextInput from '../commons/GenericTextInput';
 import GenericDatePicker from '../commons/GenericDatePicker';
@@ -97,38 +97,136 @@ export default function DialogAdquirientes(props){
 
     return(
         <CatalogDialog opened={props.opened} operacionCatalogo={mode} nombreCatalogo="Liberacion" subtitle="Liberacion de Unidades" onCancel={props.handleClose} onAccept={handleAccept}>
-            <GenericTextInput label='Fideicomiso' idCampo='funiIdFideicomiso' required={true} defaultVal='0000'/>
-            <GenericTextInput label='Número de Inmueble' idCampo='funiIdBien'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='SubCuenta' idCampo='funiIdSubcuenta'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Edificio' idCampo='funiIdSubcuenta'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Unidad Condominal' idCampo='funiIdDepto'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Niveles' idCampo='funiNiveles'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Calle' idCampo='funiCalleNum'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Colonia' idCampo='funiNomColonia'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Población' idCampo='funiNomPoblacion'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Código Postal' idCampo='funiCodigoPostal'  required={true} defaultVal='0000'/>
-            <GenericSelect labelId='País' label='País' selectId='funiNumPais' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
-            <GenericSelect labelId='Estado' label='Estado' selectId='funiNumEstado' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
-            <GenericTextInput label='Estacionamiento 1' idCampo='funiEstacionamiento1'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Estacionamiento 2' idCampo='funiEstacionamiento2'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Estacionamiento 3' idCampo='funiEstacionamiento3'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Otro' idCampo='funiRoofGarden'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Superficie Otro' idCampo='funiRoofSuperficie'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Otro 2' idCampo='funiSotano'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Superficie Otro 2' idCampo='funiSotanoSuperficie'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Indiviso' idCampo='funiIndiviso'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Registro Contable' idCampo='funiPrecio'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Precio Catastro' idCampo='funiPrecioCatastro'  required={true} defaultVal='0000'/>
-            <GenericSelect labelId='Moneda' label='Moneda' selectId='funiMoneda' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
-            <GenericSelect labelId='Estatus' label='Estatus' selectId='funiStatus' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
-            <GenericTextInput label='Acto 1' idCampo='funiActo1'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Acto 2' idCampo='funiActo2'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Acto 3' idCampo='funiActo3'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Acto 4' idCampo='funiActo4'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Notario' idCampo='funiNotario'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Escritura' idCampo='funiNumEscritura'  required={true} defaultVal='0000'/>
-            <GenericTextInput label='Folio' idCampo='funiFolioReal'  required={true} defaultVal='0000'/>
-            <GenericDatePicker idSelector='funiFechaTrasladoDominio' labelSelector='Fecha Traslativo' selectedDate={selectedDate} onChange={handleDateChangeSal}/> 
+            <Grid container spacing={1} direction="column" className={classes.rowSpacing}>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Fideicomiso' idCampo='funiIdFideicomiso' required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Número de Inmueble' idCampo='funiIdBien'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='SubCuenta' idCampo='funiIdSubcuenta'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Edificio' idCampo='funiIdSubcuenta'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Unidad Condominal' idCampo='funiIdDepto'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Niveles' idCampo='funiNiveles'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Calle' idCampo='funiCalleNum'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Colonia' idCampo='funiNomColonia'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Población' idCampo='funiNomPoblacion'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Código Postal' idCampo='funiCodigoPostal'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericSelect labelId='País' label='País' selectId='funiNumPais' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericSelect labelId='Estado' label='Estado' selectId='funiNumEstado' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Estacionamiento 1' idCampo='funiEstacionamiento1'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Estacionamiento 2' idCampo='funiEstacionamiento2'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Estacionamiento 3' idCampo='funiEstacionamiento3'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Otro' idCampo='funiRoofGarden'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Superficie Otro' idCampo='funiRoofSuperficie'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Otro 2' idCampo='funiSotano'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Superficie Otro 2' idCampo='funiSotanoSuperficie'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Indiviso' idCampo='funiIndiviso'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Registro Contable' idCampo='funiPrecio'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Precio Catastro' idCampo='funiPrecioCatastro'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericSelect labelId='Moneda' label='Moneda' selectId='funiMoneda' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericSelect labelId='Estatus' label='Estatus' selectId='funiStatus' selected={state.periodicidad} onChange={handleChangePeriodi}  /> 
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Acto 1' idCampo='funiActo1'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Acto 2' idCampo='funiActo2'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Acto 3' idCampo='funiActo3'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Acto 4' idCampo='funiActo4'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Notario' idCampo='funiNotario'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Escritura' idCampo='funiNumEscritura'  required={true} defaultVal='0000'/>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.rowSpacing} spacing={3}>
+                    <Grid item xs={6}>
+                        <GenericTextInput label='Folio' idCampo='funiFolioReal'  required={true} defaultVal='0000'/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <GenericDatePicker idSelector='funiFechaTrasladoDominio' labelSelector='Fecha Traslativo' selectedDate={selectedDate} onChange={handleDateChangeSal}/>
+                    </Grid>                        
+                </Grid>
+            </Grid>
         </CatalogDialog>
     );
 }
