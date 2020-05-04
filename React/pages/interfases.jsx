dffe7acbfa03386f0ce675ad7427ba792d469369
Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>
2020-05-04 16:14:43 -0500

    Se Agregan las nuevas pantallas de Apertura/Cierre e Interfases
    Signed-off-by: Eduardo Yair Cuéllar Rivera <eycr.nm@gmail.com>

---
+++ React/pages/interfases.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+import Layout from '../components/Layout';
+import GenericTabPanel from '../components/commons/GenericTabPanel';
+import MainInterfases from '../components/Interfases/MainInterfases';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex'
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+  tabsContentWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '80%',
+    '& > div' : {
+        width: '100%'
+    }
+  }
+}));
+
+export default function ModuloInterfases() {
+    const classes = useStyles();
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Layout>
+            <div className={classes.root}>
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    className={classes.tabs}
+                >
+                    <Tab label="Interfases"/>
+                </Tabs>
+                <div className={classes.tabsContentWrapper}>
+                    <GenericTabPanel value={value} index={0}>
+                        <MainInterfases/>
+                    </GenericTabPanel>
+                </div>
+            </div>
+        </Layout>
+    );
+};;
