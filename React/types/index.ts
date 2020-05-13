bae142832fb725b2e0a7c6e9d5971e44acb5f81e
emohedano <emohedano@switchfly.com>
2020-05-12 22:35:11 -0500

    Se crea interfaz para CatalogDialog

--- React/types/index.ts
+++ React/types/index.ts
@@ -1,4 +1,16 @@
+import { IModel } from '../models/BaseModel';
+import { OperacionesCatalogo } from '../constants';
+
 export type TabsLayout = {
     label: String;
     component: React.ReactType;
 };
+
+export interface ICatalogDialog<T extends IModel> {
+    mode: OperacionesCatalogo;
+    model: T;
+    open: boolean;
+    onClose: () => void;
+    onSaveRequest: () => void;
+    onModelFieldChange: (field: keyof T) => (value: any) => void;
+}
