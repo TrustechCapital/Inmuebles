dcd307519ffb7e9c9e17793f86a4cc74fc97cc79
emohedano <emohedano@switchfly.com>
2020-05-13 00:42:16 -0500

    Fix de error de catalogos

--- React/pages/_app.tsx
+++ React/pages/_app.tsx
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
 
 import Layout from '../sharedComponents/Layout';
 import { catalogsApi } from '../core/api';
 
 function App({ Component, pageProps }: AppProps) {
+    const [loadingApp, setLoadingApp] = useState(true);
+
     useEffect(() => {
-        catalogsApi.fetchAllCatalogs();
+        catalogsApi.fetchAllCatalogs().then((params) => {
+            setLoadingApp(false);
+        });
     }, []);
 
     return (
         <Layout>
-            <Component {...pageProps} />
+            {loadingApp ? 'Cargando...' : <Component {...pageProps} />}
         </Layout>
     );
 }
