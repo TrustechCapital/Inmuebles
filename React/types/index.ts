6b23bc2e5472cb095a3e7745446b49ceb8b7335e
emohedano <emohedano@switchfly.com>
2020-05-23 01:15:44 -0500

    Se agregan propiedades comunes a IDialogCatalog

--- React/types/index.ts
+++ React/types/index.ts
@@ -1,5 +1,5 @@
 import { IModel } from '../models/BaseModel';
-import { OperacionesCatalogo } from '../constants';
+import { OperacionesCatalogo, SavingStatus } from '../constants';
 
 export type TabsLayout = {
     label: String;
@@ -10,6 +10,9 @@ export interface ICatalogDialog<T extends IModel> {
     mode: OperacionesCatalogo;
     model: T;
     open: boolean;
+    savingStatus: SavingStatus;
+    isLoading: boolean;
+    errorMessage: string | null;
     onClose: () => void;
     onSaveRequest: (model: T) => void;
 }
