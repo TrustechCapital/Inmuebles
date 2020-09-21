e76df60a0e29a1b3d1c227c50dd0a2bbcb1485fe
emohedano <emohedano@switchfly.com>
2020-09-21 15:37:29 -0500

    Frontend: Se configura entorno para jboss

--- React/core/api.config.ts
+++ React/core/api.config.ts
@@ -2,6 +2,6 @@ export const apiConfig = {
     returnRejectedPromiseOnError: true,
     //withCredentials: true,
     timeout: 30000,
-    baseURL: 'http://127.0.0.1:7101/FiduciaWeb',
+    baseURL: '/FiduciaBackend',
     headers: {},
 };
--- React/next.config.js
+++ React/next.config.js
@@ -7,4 +7,5 @@ module.exports = {
             '/': { page: '/' },
         };
     },
+    assetPrefix: '/FiduciaWeb'
 };
